0f1b884325c2977e9cc2eb2acbc53d3de0171e7f
Bridged1707 <shinkensei374@gmail.com>
2026-03-13 02:59:19 +0000

    push

--- ucte/main.py
+++ ucte/main.py
@@ -6,11 +6,13 @@ from ucte.collectors.shop_snapshot import run as shop_snapshot_run
 
 async def main():
 
+    queue = asyncio.Queue()
+
     print("[UCTE] Connecting to economy websocket")
 
     await shop_snapshot_run()
 
-    await websocket_run()
+    await websocket_run(queue)
 
 
 if __name__ == "__main__":
